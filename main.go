fe3ebbdf159b5345a4e78b5cfa20c6d3b68b95e3
v1ncee <duchateau.vince@gmail.com>
2025-12-03 15:51:29 +0100

    read in chunks of 32

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("Error opening file", err)
+		return
+	}
+
+	defer f.Close() // Closes when main closes
+
+	buf := make([]byte, 8)
+
+	for {
+		n, err := f.Read(buf)
+
+		if n > 0 {
+			fmt.Println("read:", string(buf[:n]))
+		}
+
+		if err == io.EOF {
+			return
+		}
+
+		if err != nil {
+			fmt.Println("Error reading file", err)
+			return
+		}
+	}
 }
